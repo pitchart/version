c91788b31368fe542b2e71dccf55bbfecb110235
Julien VITTE <vitte.julien@gmail.com>
2017-01-01 23:03:03 +0100

    [FIX] typo in exception message

--- src/VersionException.php
+++ src/VersionException.php
@@ -6,6 +6,6 @@ class VersionException extends \InvalidArgumentException
 {
 
     public static function invalidVersionNumber($number) {
-        return new self($number.'is not a valid semantic versioning number');
+        return new self($number.' is not a valid semantic versioning number');
     }
 }
